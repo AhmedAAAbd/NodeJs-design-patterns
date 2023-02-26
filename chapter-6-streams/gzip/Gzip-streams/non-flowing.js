d26d25c701548af71e81786f9aed87c7a9a5bafe
Ahmed Ali <ahmedali@Ahmeds-Mac-mini.lan>
2023-02-26 13:46:05 -0500

    flowing mode

--- chapter-6-streams/gzip/Gzip-streams/non-flowing.js
+++ chapter-6-streams/gzip/Gzip-streams/non-flowing.js
@@ -1,6 +1,18 @@
-process.stdin.on("readable", () => {
-	let chunk;
-	while ((chunk = process.stdin.read()) !== null) {
+//non-flowing mode
+// process.stdin.on("readable", () => {
+// 	let chunk;
+// 	while ((chunk = process.stdin.read()) !== null) {
+// 		// Read data from stdin is synchronous operation that pulls data from the internal buffers of readable stream. this returned data is a buffer object. if the stream is working in binary mode.
+// 		console.log(`Chunk read (${chunk.length} bytes): ${chunk.toString()}`);
+// 	}
+// });
+
+//flowing mode
+process.stdin
+	.on("data", (chunk) => {
+		console.log("New data available");
 		console.log(`Chunk read (${chunk.length} bytes): ${chunk.toString()}`);
-	}
-});
+	})
+	.on("end", () => {
+		console.log("No more data");
+	});
