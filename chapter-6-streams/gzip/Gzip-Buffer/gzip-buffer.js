b921f867eca987a0b171f60de0429bb496a137c2
Ahmed Ali <ahmedali@Ahmeds-Mac-mini.lan>
2023-02-25 22:18:21 -0500

    final

--- chapter-6-streams/gzip/Gzip-Buffer/gzip-buffer.js
+++ chapter-6-streams/gzip/Gzip-Buffer/gzip-buffer.js
@@ -1,15 +1,15 @@
-import { createGzip } from "zlib";
+import { gzip } from "zlib";
 import { promisify } from "es6-promisify";
 import { promises as fs } from "fs";
 
-const gzip = promisify(createGzip());
+const createGzip = promisify(gzip);
 
 const filename = process.argv[2];
 
 async function main() {
 	const data = await fs.readFile(filename);
-	const compressed = await gzip(data);
-	fs.writeFile(filename + ".gz", compressed);
+	const compressed = await createGzip(data);
+	await fs.writeFile(filename + ".gz", compressed);
 	console.log("Compressed file written to " + filename + ".gz");
 }
 main();
